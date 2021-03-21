2b4c8db30091c8844daac1b4bf7f48f029344fbf
joshuabmarsh <48936776+joshuabmarsh@users.noreply.github.com>
2021-03-21 12:37:46 +1100

    Add files via upload

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,21 +36,20 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["AUTO", "WEBP", "AVIF"]}
-        // src="../images/profile-pic.png"
+        src="../images/profile-pic.png"
         width={50}
         height={50}
         quality={95}
         alt="Profile picture"
       />
-// i dont want to use this so ignoring it
-      // {author?.name && (
-      //  <p>
-      //    Written by <strong>{author.name}</strong> {author?.summary || null}
-      //    {` `}
-      //    <a href={`https://twitter.com/${social?.twitter || ``}`}>
-      //      You should follow them on Twitter
-      //    </a>
-      //  </p>
+       {author?.name && (
+        <p>
+          Written by <strong>{author.name}</strong> {author?.summary || null}
+          {` `}
+          <a href={`https://twitter.com/${social?.twitter || ``}`}>
+            You should not follow them on Twitter
+          </a>
+        </p>
       )}
     </div>
   )
